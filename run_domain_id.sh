9d2e797f7a2d83ae298a3c33d7008ef029b42814
ketina-mirror <ririhedou@gmail.com>
2017-12-01 10:42:16 -0500

    server

---
+++ run_domain_id.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Run the code"
+python2 history_scan.py 17
+wait
+
+echo "Sleep"
+sleep 10
+
+python2 history_scan.py 27
+wait
+
+python2 history_scan.py 28
+wait
+
+python2 history_scan.py 29
+wait
+
---
+++ run_domain_id_bishop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Run the code"
+python2 history_scan.py 59
+wait
+
+echo "Sleep"
+sleep 10
+
+python2 history_scan.py 60
+wait
+
